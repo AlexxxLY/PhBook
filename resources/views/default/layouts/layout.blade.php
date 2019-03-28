868f7f67437350227fad9066d329e6ebef5ece85
Alex Lyapko <alexxx.ax4@gmail.com>
2019-03-28 14:41:24 +0200

    add model Phone _contact

--- resources/views/default/layouts/layout.blade.php
+++ resources/views/default/layouts/layout.blade.php
@@ -80,21 +80,7 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    PhoneBook
-                </div>
-
-                <!-- <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> -->
-            </div>
+            @yield('content')
         </div>
     </body>
 </html>
